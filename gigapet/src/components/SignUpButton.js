6bcc46387e7a0a62412d0d7dfb87ab9642ee1b7d
dominique maack <dominiquemaack@gmail.com>
2019-10-21 16:53:15 -0600

    commented out extra handleSubmit, added icon images, added icons to addFoodLaunch

--- gigapet/src/components/SignUpButton.js
+++ gigapet/src/components/SignUpButton.js
@@ -13,7 +13,7 @@ import {Link} from "react-router-dom";
 const SignUpButton = () => {     
    
     return ( 
-        <div class="buttonContainer">
+        <div className="buttonContainer">
             <Link to="/signup">
                 <button>Sign Up</button>
             </Link>
