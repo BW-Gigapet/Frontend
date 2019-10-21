84ca1e2cb88cc7c205f9c44e217f0a506a8984cb
dominique maack <dominiquemaack@gmail.com>
2019-10-21 14:44:39 -0600

    added more placeholders for left dashboard components

--- gigapet/src/App.js
+++ gigapet/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 // import DailyProgressSummary from './components/DailyProgressSummary';
-// import AddFoodEntry from './components/AddFoodEntry';
+// import AddFoodEntry from './components/AddFoodLaunch';
 
 function App() {
   return (
     <div className="App">
-    
+      {/* <DailyProgressSummary />
+      <AddFoodEntry /> */}
     </div>
   );
 }
