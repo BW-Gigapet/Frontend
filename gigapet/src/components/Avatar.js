4ac1ef03e31ad8635951716437cf1068db8d509a
ericbanker12 <ericbanker12@gmail.com>
2019-10-22 12:03:35 -0500

    add more animations to avatar

--- gigapet/src/components/Avatar.js
+++ gigapet/src/components/Avatar.js
@@ -19,6 +19,20 @@ const waveRight = keyframes`
     transform: translateX(-50%) translateY(10%) rotate(40deg);
 }
 `
+const waveLeftTired = keyframes`
+50% {
+    transform: translateX(-50%) translateY(10%) rotate(20deg);
+}
+`
+
+const waveRightTired = keyframes`
+50% {
+    transform: translateX(-50%) translateY(10%) rotate(-20deg);
+}
+`
+
+const blink = keyframes`50% { opacity: 0.33;}`
+
 
 const Container = styled.div`position:relative;`
 
@@ -35,6 +49,7 @@ position: absolute;
 z-index: 3;
 left: 50%;
 transform: translateX(-50%) translateY(10%);
+display: ${props => props.hide?'none':'unset'};
 `
 
 const BearArmLeft = styled.img`
@@ -44,26 +59,38 @@ z-index: 2;
 left: 50%;
 transform: translateX(-50%) translateY(10%) rotate(60deg);
 transform-origin: 65% 45%;
-animation: ${waveLeft} 2s;
+animation: ${props => props.animation} ${props => props.speed};
 animation-iteration-count: infinite;
 `
 
 const BearArmRight = styled(BearArmLeft)`
 transform: translateX(-50%) translateY(10%) rotate(-60deg);
 transform-origin: 35% 45%;
-animation: ${waveRight} 2s;
+`
+
+const Snoring = styled.span`
+font-size: 4vw;
+font-weight: bold;
+display: ${props => props.hide?'none':'block'};
+position: absolute;
+z-index: 4;
+left: 50%;
+text-shadow: 2px 2px 0 white, -2px 2px 0px white, 2px -2px 0px white, -2px -2px 0px white;
+animation: ${blink} 3s;
 animation-iteration-count: infinite;
 `
 
 export function Avatar(props) {
+    // change animation to class, so play on click
     return (
         <Container>
             <Backgorund src={bearBG} />
             <BearBody src={bearBody} />
-            {/* <BearBody src={tired} /> */}
-            {/* <BearBody src={sleeping} /> */}
-            <BearArmLeft src={leftArm} />
-            <BearArmRight src={rightArm} />
+            <BearBody src={tired} hide={true} />
+            <BearBody src={sleeping} hide={true} />
+            <BearArmLeft src={leftArm} animation={waveLeft} speed='1s' />
+            <BearArmRight src={rightArm} animation={waveRight} speed='1s' />
+            <Snoring hide={true}>zZZ</Snoring>
         </Container>
     )
 }
