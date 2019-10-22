ad52ffbd7e4719f791675b5793487be4c4f8d6ea
ericbanker12 <ericbanker12@gmail.com>
2019-10-22 16:41:24 -0500

    add comment to Summary.js

--- gigapet/src/components/Summary.js
+++ gigapet/src/components/Summary.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import axiosWithAuth from '../utils/axiosWithAuth'
 
 import { SelectCategory, CategoryBanner, SummaryData } from './index'
 
 export class Summary extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            category: 'Vegetables',
+        }
     }
 
     componentDidMount() {
         //axios get data
+        // axiosWithAuth().get(`/api/child/:id/meals?filter=prevThirty`)
+        //     .then(resp => {
+        //         console.log(resp)
+        //     })
+        //     .catch(err => console.log(err.response))
     }
 
     componentWillUnmount() {
