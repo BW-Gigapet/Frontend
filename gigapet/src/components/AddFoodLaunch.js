f6c450f9a673d9431d012123ef5cbd79095bcb1c
dominique maack <dominiquemaack@gmail.com>
2019-10-21 22:35:08 -0600

    progress bars rendering and ready for state changes, added placeholder styling for bottom left component in Dashboard, added more files + skeletons

--- gigapet/src/components/AddFoodLaunch.js
+++ gigapet/src/components/AddFoodLaunch.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import vegImage from '../assets/VegetableIcon.png';
 import fruitImage from '../assets/FruitsIcon.png';
 import grainImage from '../assets/GrainIcon.png';
@@ -17,28 +18,46 @@ const AddFoodLaunch = () => {
     return (
         <div>
             <div className='add-food-btn'>
-                <button> + </button>
+                <AddButton> + </AddButton>
             </div>
-            <div className='add-food-icons'>
-                <button>
-                    <img src={ vegImage } alt='vegetable icon' />
-                </button>
-                <button>
-                    <img src={ fruitImage } alt='fruit icon' />
-                </button>
-                <button>
-                    <img src={ grainImage } alt='grain icon' />
-                </button>
-                <button>
-                    <img src={ proteinImage } alt='protein icon' />
-                </button>
-                <button>
-                    <img src={ fatsImage } alt='fats and oils icon' />
-                </button>
-                <button>
+            <Icons className='add-food-icons'>
+                <IconContainer>
+                    <Buttons>
+                        <VegImage src={ vegImage } alt='vegetable icon' />
+                    </Buttons>
+                    <p>Vegetable</p>
+                </IconContainer>
+                <IconContainer>
+                    <Buttons>
+                        <img src={ fruitImage } alt='fruit icon' />
+                    </Buttons>
+                    <p>Fruits</p>
+                </IconContainer>
+                <IconContainer>
+                    <Buttons>
+                        <img src={ grainImage } alt='grain icon' />
+                    </Buttons>
+                    <p>Whole Grains</p>
+                </IconContainer>
+                <IconContainer>
+                    <Buttons>
+                        <img src={ proteinImage } alt='protein icon' />
+                    </Buttons>
+                    <p>Protein</p>
+                </IconContainer>
+                <IconContainer>
+                    <Buttons>
+                        <img src={ fatsImage } alt='fats and oils icon' />
+                    </Buttons>
+                    <p>Fats and Oils</p>
+                </IconContainer>
+                <IconContainer>
+                <Buttons>
                     <img src={ sugarsImage } alt='sugars icon' />
-                </button>
-            </div>
+                </Buttons>
+                <p>Sugars</p>
+                </IconContainer>
+            </Icons>
 
         </div>
     )
@@ -46,4 +65,42 @@ const AddFoodLaunch = () => {
 
 export default AddFoodLaunch;
 
-// const VegImage = styled.components
+const Icons = styled.div`
+    width: 500px;
+    height: 800px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+
+const IconContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-size: .5rem;
+`;
+
+const AddButton = styled.button`
+    width: 40px;
+    height: 40px;
+    background-color: #247CAD;
+    color: white;
+    border-radius: 50%;
+    font-size: 2rem;
+    border: none;
+`;
+
+const Buttons = styled.button`
+    width: 70px;
+    height: 70px;
+    text-decoration: none;
+    border: none;
+    padding: 0;
+    background-color: #EBF9EF;
+`;
+
+const VegImage = styled.img`
+    width: 50px;
+    height: 50px;
+    // padding: 3%;
+    // background-color: #EBF9EF;
+`;
--- gigapet/src/components/DailyProgressSummary.js
+++ gigapet/src/components/DailyProgressSummary.js
@@ -2,12 +2,36 @@ import React from 'react';
 import DailyProgressCategory from './DailyProgressCategory';
 
 const categories = [
-    'Vegetable',
-    'Fruit',
-    'Whole Grains',
-    'Protein',
-    'Fats & Oils',
-    'Sugars'
+    {
+        name: 'Vegetable',
+        progressBar: '#EBF9EF',
+        fillerColor: '#82D99C'
+    },
+    {
+        name: 'Fruit',
+        progressBar: '#F7F5DE',
+        fillerColor: '#DBCF61'
+    },
+    {
+        name: 'Whole Grains',
+        progressBar: '#FDEDE7',
+        fillerColor: '#F38058'
+    },
+    {
+        name: 'Protein',
+        progressBar: '#EAF5FB',
+        fillerColor: '#5BAEDC'
+    },
+    {
+        name: 'Fats and Oils',
+        progressBar: '#EFEAFA',
+        fillerColor: '#9678DE'
+    },
+    {
+        name: 'Sugars',
+        progressBar: '#FFEBF6',
+        fillerColor: '#FF66B9'
+    }
 ]
 
 
@@ -19,7 +43,7 @@ const DailyProgressSummary = (props) => {
             <button>Edit</button>
             {/* map categories */}
             {categories.map(category => (
-                <DailyProgressCategory category={category} />
+                <DailyProgressCategory /*key={}*/ category={category} />
             ))}
             
         </div>
