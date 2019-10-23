b1d5b4a1014af42c8d9ca4f0685a2555509c79d3
dominique maack <dominiquemaack@gmail.com>
2019-10-22 22:57:56 -0600

    refactored signUp from having nested axios call to calling getLoggedInUser action to re-use, connected all forms to rredux, connecting dashboard to login and signup via history, progress bar functionality done, and mocked up data to edit form and can add items now

--- gigapet/src/actions/index.js
+++ gigapet/src/actions/index.js
@@ -1,15 +1,18 @@
 import { axiosWithAuth } from '../utils/axiosWithAuth';
+import KidsProfileSetup from '../components/KidsProfileSetup';
 
 export const FETCH_MEALS_START = 'FETCH_MEALS_START';
 export const FETCH_MEALS_SUCCESS = 'FETCH_MEALS_SUCCESS';
 export const FETCH_MEALS_FAILURE = 'FETCH_MEALS_FAILURE';
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
-
-export const fetchMeals = () => dispatch => {
+export const fetchMeals = (childId) => dispatch => {
     dispatch({ type: FETCH_MEALS_START })
-
+    if (childId ) {
     axiosWithAuth()
-    .get('/api/meals/')
+    .get(`/api/child/${childId}/meals?filter=yesterday`)
     .then(res => {
         console.log('response from GET meals', res)
         dispatch({ type: FETCH_MEALS_SUCCESS, payload: res.data })
@@ -17,5 +20,24 @@ export const fetchMeals = () => dispatch => {
     .catch(err => {
         console.log('No data', err)
         dispatch({ type: FETCH_MEALS_FAILURE, payload: err })
+    })
+}
+}
+
+
+export const getLoggedInUser = () => dispatch => {
+    dispatch({ type: LOGIN_START })
+
+    axiosWithAuth()
+    .get('/api/users')
+    .then(res => {
+      console.log('response from GET id Kid', res)
+      dispatch({ type: LOGIN_SUCCESS, payload: res.data.loggedInUser })
+      //setLoggedInUser(res.data.loggedInUser)
+      
+    }) 
+    .catch(err => {
+      console.log('could not render data', err)
+      dispatch({ type: LOGIN_FAILURE, payload: err })
     })
 }
