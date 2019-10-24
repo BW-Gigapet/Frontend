a9a896328ccf1e0a7b289d018746e16ee435912a
danRolon <daniel.rolon@yahoo.com>
2019-10-24 17:50:09 -0500

    added function to load the reduxstore from local storage if it exists and to save the store to localstorage on page exit/refresh

--- gigapet/src/index.js
+++ gigapet/src/index.js
@@ -10,14 +10,50 @@ import thunk from 'redux-thunk';
 import { logger } from 'redux-logger';
 import { reducer } from './reducers';
 
+const loadState = () => {
+    console.log('loading state....')
+	try {
+		const serializedState = localStorage.getItem('state');
+        if(serializedState === null)
+        {
+            console.log('no state saved to load....')
+			return undefined;
+        }
+        console.log('loaded state....', JSON.parse(serializedState))
+		return JSON.parse(serializedState);
+	} catch(err){
+        console.log('loading state failed....')
+		return undefined;
+	}
+}
+
+const saveState = state =>{
+    console.log('saving state....')
+	try{
+        const serializedState = JSON.stringify(state);
+		localStorage.setItem('state', serializedState)
+        console.log('saved state....')
+	} catch (err) {
+		console.log('saving state failed....', err)
+	}
+}
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistedState = loadState();
+
 const store = createStore( 
-    reducer, 
+    reducer,
+    persistedState,
     composeEnhancers(applyMiddleware(thunk, logger))
 );
 
+// store.subscribe(() => {
+// 	saveState(store.getState());
+// });
+window.addEventListener('beforeunload', () =>{
+    saveState(store.getState());
+});
 
 ReactDOM.render(
 <Provider store={ store } >
